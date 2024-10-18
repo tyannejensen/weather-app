db848b8192f6033becbe46b5ac41ba567f38f06c
Tyanne Jensen <vibewithme1123@Tyannes-Laptop.local>
2024-10-17 22:52:42 -0600

    serving static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
+import path from 'path';
 
 // Import the routes
 import routes from './routes/index.js';
 
 const app = express();
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 const PORT = process.env.PORT || 3001;
 
